2012d17bcd60fbc1858b82c683bc3bfd2b1febd2
szymonkn <szymon.skn@gmail.com>
2021-05-22 14:53:35 +0200

    Section 3 -> completed

--- Unit 2/TipCalcApp/app/src/main/java/com/example/tipcalcapp/MainActivity.kt
+++ Unit 2/TipCalcApp/app/src/main/java/com/example/tipcalcapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.tipcalcapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioGroup
@@ -17,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfService.setOnKeyListener { view, keyCode, _ -> handleKey(view,keyCode) }
        /* val editTextCost : EditText = findViewById(R.id.cost_of_service)
         val radioGroup : RadioGroup = findViewById(R.id.radioGroup)
         val calculateButton : Button = findViewById(R.id.calculate_button)
@@ -30,6 +34,8 @@ class MainActivity : AppCompatActivity() {
     }
     private fun calculateTip(){
         val stringInTextField = binding.costOfService.text.toString()
+        if(stringInTextField == "")
+            return
         val cost = stringInTextField.toDouble()
         val tipPercentage = when(binding.radioGroup.checkedRadioButtonId){
             R.id.option_twenty_percent -> 0.20
@@ -42,6 +48,14 @@ class MainActivity : AppCompatActivity() {
 
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_textView,formattedTip)
+    }
+    private fun handleKey(view: View,keyCode:Int):Boolean{
+        if(keyCode == KeyEvent.KEYCODE_ENTER){
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+            return true
+        }
+        return false
     }
 
 }
